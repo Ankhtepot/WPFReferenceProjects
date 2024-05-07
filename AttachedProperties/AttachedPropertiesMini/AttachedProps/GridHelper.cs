fadb01940ebf6bc68c6f39001e63d542fc57cbe3
Zavodny Petr <petr.zavodny@sensearena.com>
2024-05-07 19:59:01 +0200

    AttachedProperty mini solution

---
+++ AttachedProperties/AttachedPropertiesMini/AttachedProps/GridHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace AttachedProps;
+
+using System.Windows;
+using System.Windows.Controls;
+
+public static class GridHelper
+{
+    public static readonly DependencyProperty ColumnSpanProperty =
+        DependencyProperty.RegisterAttached(
+            "ColumnSpan", typeof(int), typeof(GridHelper),
+            new PropertyMetadata(1, OnColumnSpanChanged));
+
+    public static int GetColumnSpan(DependencyObject obj)
+    {
+        return (int)obj.GetValue(ColumnSpanProperty);
+    }
+
+    public static void SetColumnSpan(DependencyObject obj, int value)
+    {
+        obj.SetValue(ColumnSpanProperty, value);
+    }
+
+    private static void OnColumnSpanChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is UIElement element and FrameworkElement)
+        {
+            Grid.SetColumnSpan(element, (int)e.NewValue);
+        }
+    }
+}
